156b6833c70f1023de7b4474819c89a81c0c47c0
zalina-devops <z_cesi@outlook.com>
2026-03-09 01:49:27 +0500

    fix postgres connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+const connectionString = process.env.DATABASE_URL;
+
+console.log("DATABASE_URL:", connectionString);
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: connectionString,
 });
 
 module.exports = pool;
