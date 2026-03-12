241edf72979b5a71541e74bf2668f0dc354781e0
zalina-devops <z_cesi@outlook.com>
2026-03-12 18:07:28 +0500

    fix deploy config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'personal-finance-client-server',
+  base: '/personal-finance-client-server/',
 })
