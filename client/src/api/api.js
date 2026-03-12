6f4aa365c644b23e05134e16445c85c80e7c9216
zalina-devops <z_cesi@outlook.com>
2026-03-12 17:51:31 +0500

    add deploy config

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.0.55:5000/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
